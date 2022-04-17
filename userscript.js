3f6c1c3f593cabcc447baf32a6a1d0891b68167a
CatgirlCultist <44424202+CatgirlCultist@users.noreply.github.com>
2022-04-17 16:48:39 -0700

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -2,7 +2,7 @@
 // @name         Issue number display
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Displays issue numbers to be consistent with http://www.mwq.dds.nl/ns/results/ on the button
+// @description  Displays issue answer numbers to be consistent with http://www.mwq.dds.nl/ns/results/ on the button
 // @author       Seanat
 // @match        https://www.nationstates.net/page=show_dilemma/dilemma=*
 // @icon         https://www.google.com/s2/favicons?domain=nationstates.net
